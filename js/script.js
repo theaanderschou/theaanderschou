0091293a92287ef3644e39aea13880c774d1d0f1
theaanderschou <74912677+theaanderschou@users.noreply.github.com>
2021-09-26 01:18:15 +0200

    baaaah hero

--- js/script.js
+++ js/script.js
@@ -43,26 +43,26 @@ let timeline = new TimelineMax();
 
 timeline
   .to(".god", 10, { y: -300 })
-  .to(".like", 10, { y: -100 }, "-=10")
+  .to(".like", 10, { y: -200 }, "-=10")
   .to(".fish", 10, { y: -300 }, "-=10")
   .to(".gun", 10, { y: -400 }, "-=10")
   .to(".like", 10, { y: -200 }, "-=10")
   .to(".shadow", 10, { y: -400 }, "-=10")
+
   .to(".main-title", 10, { y: -600 }, "-=10")
   .to(".undertitle", 10, { y: -600 }, "-=10")
-//  .to(".fight", 10, { y: -200 }, "-=10")
-//  .to(".femur", 10, { y: -100 }, "-=10")
+
   .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
   .to(".content", 10, { top: "0%" }, "-=10")
-  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+//  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
   .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
 
   let scene = new ScrollMagic.Scene({
-    triggerElement: "section",
+    triggerElement: "#hero",
     duration: "300%",
-    triggerHook: 0,
+    triggerHook: 0.1,
   })
     .setTween(timeline)
-    .setPin("section")
+    .setPin("#hero")
     .addTo(controller);
   
